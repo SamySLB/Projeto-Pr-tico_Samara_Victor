d96fe8394e0f5661e187db15562d576b663ccb8a
SAMYSBL <samara.barcelos@live.com>
2025-11-05 15:19:12 -0300

    adicionado tela inicial na main e criando rota principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
+import 'screens/intro_screen.dart';
 
 void main() {
-  runApp(const MeuApp());
+  runApp(const PokeApp());
 }
 
-class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
+class PokeApp extends StatelessWidget {
+  const PokeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meu Primeiro App Flutter',
+      title: 'PokeApp',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Olá, Flutter!'),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: const Center(
-          child: Text(
-            'Bem-vinda ao seu primeiro app 🎉',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
       ),
+      home: const IntroScreen(),
+      routes: {
+        '/home': (context) => const Placeholder(), //aqui é a rota esperando proxima tela
+      },
     );
   }
 }
-
