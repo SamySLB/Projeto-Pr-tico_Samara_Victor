ec1e15112cc4f61675fc755bdf44763d65741f0b
Victor Gomes <victorgomes7f@gmail.com>
2025-11-20 19:55:14 -0300

    Update user_session.dart
    modificações finais

--- lib/utils/user_session.dart
+++ lib/utils/user_session.dart
@@ -2,7 +2,7 @@ class UserSession {
   static final UserSession instance = UserSession._();
   UserSession._();
 
-  String email = "";
+  String email = "E-mail";
   String senha = "";
   String nome = "Nome Completo";
 }
